4d6f437f1a7b9cf367fe2375de13e5bacb07aa72
Matthieu Kozicki <matthieu.kozicki@epitech.eu>
2018-11-19 13:17:14 +0100

    Changing main

--- main.c
+++ main.c
@@ -16,11 +16,11 @@
 char **mem_alloc_2d_array(int nb_rows, int nb_cols)
 {
     int i = 0;
-    char **result = malloc(sizeof(char *) * (nb_cols + 2));
+    char **result = malloc(sizeof(char *) * (nb_rows + 2));
 
-    result[nb_cols + 1] = NULL;
+    result[nb_rows + 1] = NULL;
     for (i = 0; i <= nb_rows; i++) {
-        result[i] = malloc(sizeof(char*) * (nb_rows + 2));
+        result[i] = malloc(sizeof(char) * (nb_cols + 2));
         result[i][nb_rows + 1] = '\0';
     }
     return (result);
